bf0ccbdab6035365fa09a487c8dbd79d2f64f3dd
Minksthesinks2 <135671183+Minksthesinks2@users.noreply.github.com>
2023-06-05 20:17:46 -0700

    Add files via upload

---
+++ War with SCoreboard.srcs/sources_1/new/Accumulator.sv
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer: Bridget Benson 
+// Create Date: 10/26/2018 12:57:18 PM
+// Description: 8 bit acculumator.  Adds new value to the
+// current value when LD is 1.  
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Accumulator(
+    input clk, LD, CLR,
+    input [7:0] D,
+    output logic [7:0] Q
+    );
+    
+    always_ff @ (posedge clk, posedge CLR)
+    begin
+        if (CLR)
+            Q = 0;
+        else if (LD)
+            Q = D + Q;
+         
+    
+    end
+endmodule
