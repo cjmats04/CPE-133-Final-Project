1be42dddafe98ad7235b48908fea3b5a3d518741
Calvin Matsushita <cjmats04@gmail.com>
2023-06-05 19:53:50 -0700

    created

---
+++ Final_Project_Team/Final_Project_Team.srcs/sources_1/new/FPT.sv
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/05/2023 07:51:46 PM
+// Design Name: 
+// Module Name: FPT
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module FPT(
+    input clk,
+    input [3:0] FPT_A,
+    input [3:0] FPT_B,
+    input FPT_ENTER,
+    output [7:0] FPT_SSEG,
+    output [3:0] FPT_An,
+    output [1:0] FTP_WLED
+    );
+endmodule
